1afe34ed4cdd207432b16291954292b4036b003d
jocarlop <jocarlop@espol.edu.ec>
2019-12-31 11:35:57 -0500

    Commit inicial

--- app/src/androidTest/java/com/example/pst_ta5_g7/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/pst_ta5_g7/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.pst_ta5_g7;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- app/src/main/java/com/example/pst_ta5_g7/MainActivity.java
+++ app/src/main/java/com/example/pst_ta5_g7/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pst_ta5_g7;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
